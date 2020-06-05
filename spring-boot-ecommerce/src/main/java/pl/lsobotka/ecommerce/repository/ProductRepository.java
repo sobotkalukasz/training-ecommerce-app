3b6466759ad1cf203f6534a341d241d1b763c4b0
sobotkalukasz <sobotka.lukasz@gmail.com>
2020-06-05 21:38:20 +0200

    Enabled CORS - hardcoded for api/products endpoint

--- spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package pl.lsobotka.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.lsobotka.ecommerce.model.Product;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
