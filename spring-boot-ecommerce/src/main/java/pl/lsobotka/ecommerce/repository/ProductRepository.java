af2487c534e56113826f010320063223d7a29940
sobotkalukasz <sobotka.lukasz@gmail.com>
2020-06-08 19:14:46 +0200

    Exposed 'findByCategoryId' search product REST endpoint

--- spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package pl.lsobotka.ecommerce.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.lsobotka.ecommerce.model.Product;
 
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
