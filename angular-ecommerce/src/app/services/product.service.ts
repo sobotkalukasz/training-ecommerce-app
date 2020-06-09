2867c0d066e831cb40ac15a659761c4ff1b8df40
sobotkalukasz <sobotka.lukasz@gmail.com>
2020-06-09 21:24:59 +0200

    Added: * product-category class to represent product categories * product-category-menu component for dynamic side menu * support for product categories in product service

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -3,25 +3,41 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../common/product';
 import { map } from 'rxjs/operators';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private baseUrl = "http://localhost:8080/api/products";
+  private baseUrl = "http://localhost:8080/api";
+  private productUrl = `${this.baseUrl}/products`;
+  private productCategoryUrl = `${this.baseUrl}/product-category`;
+  
   constructor(private httpClient: HttpClient) { }
 
   getProductList(categoryId: number): Observable<Product[]> {
-    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
-    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+    const searchUrl = `${this.productUrl}/search/findByCategoryId?id=${categoryId}`;
+    return this.httpClient.get<GetProductResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
+    );
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+        return this.httpClient.get<GetProductCategoryResponse>(this.productCategoryUrl).pipe(
+      map(response => response._embedded.productCategory)
     );
   }
 }
 
-interface GetResponse {
+interface GetProductResponse {
   _embedded: {
     products: Product[];
+  }
+}
+
+interface GetProductCategoryResponse {
+  _embedded: {
+    productCategory: ProductCategory[];
   }
 }
