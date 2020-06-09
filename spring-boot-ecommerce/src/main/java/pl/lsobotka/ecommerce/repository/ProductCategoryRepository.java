93e800758db3b1e753caef79c81b90eaf37bfc94
sobotkalukasz <sobotka.lukasz@gmail.com>
2020-06-09 21:19:52 +0200

    Enabled CORS for ProductCategoryRepository

--- spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/pl/lsobotka/ecommerce/repository/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package pl.lsobotka.ecommerce.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import pl.lsobotka.ecommerce.model.ProductCategory;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
