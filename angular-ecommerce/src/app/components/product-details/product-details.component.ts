3acbae2ab53f4f21ae5d2b8fff297b994c4829e2
sobotkalukasz <sobotka.lukasz@gmail.com>
2020-06-14 19:20:28 +0200

    Added support to 'add to cart' for product details component

--- angular-ecommerce/src/app/components/product-details/product-details.component.ts
+++ angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +14,7 @@ export class ProductDetailsComponent implements OnInit {
 
   product: Product = new Product();
 
-  constructor(private productService: ProductService, private route: ActivatedRoute) { }
+  constructor(private productService: ProductService, private cartService: CartService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(() => {
@@ -27,6 +29,11 @@ export class ProductDetailsComponent implements OnInit {
         this.product = data;
       }
     )
+  }
+
+  addToCart(product: Product) {
+    let cartItem: CartItem = new CartItem(product);
+    this.cartService.addToCart(cartItem);
   }
 
 }
